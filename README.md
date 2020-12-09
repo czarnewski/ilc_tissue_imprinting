5ba29d597e2f0d6a41f7f0c8974f0874c27ea990
Czarnewski <paulo.czarnewski@scilifelab.se>
2020-12-09 20:56:11 +0100

    adds folders environemnt and workflow files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Tissue-specific transcriptional imprinting and heterogeneity in human innate lymphoid cells revealed by full-length single-cell RNA-sequencing
 Repository contacting the code related to the article "Tissue-specific transcriptional imprinting and heterogeneity in human innate lymphoid cells revealed by full-length single-cell RNA-sequencing" [Mazzurana, Czarnewski et al (2020) *Cell Research*](https://doi.org/10.1038/s41422-020-00445-x)
+
+
+The analysis done herein can be reproduced by installing `conda` and running the `run_workflow.sh` script. It will download the dataset from GEO (GSE150050), the scripts from Sauron.
+
+[More to information come here soon]
---
+++ run_workflow.sh
@@ -0,0 +1,219 @@
+#! /bin/bash -l
+
+##################################
+### ACTIVATE CONDA ENVIRONMENT ###
+##################################
+# conda activate base
+# conda install -c conda-forge mamba
+# mamba env create -sauron -f sauron_environment_20201209.yml
+# conda activate sauron
+
+
+
+
+
+
+########################
+### DEFINE VARIABLES ###
+########################
+var_to_plot='Dataset,Tissue,Plate,Donor,Celltype,Sex'
+var_to_regress='perc_mito,perc_rpl,perc_rps,perc_Chr_Y,perc_snRNA,Sex'
+main=$(pwd)
+cd $main
+! test -d 'analysis' && { mkdir analysis }
+! test -d 'log' && { mkdir log }
+! test -d 'data' && { mkdir data }
+
+
+
+######################################################
+### DOWNLOAD DATA FROM GEO ANS SCRIPTS FROM GITHUB ###
+######################################################
+git clone 'https://github.com/NBISweden/sauron.git'
+script_path=$main/'sauron/scripts'
+
+cd $main/'data'
+curl -O https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE150050&format=file&file=GSE150050%5Fmetadata%2Ecsv%2Egz
+curl -O https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE150050&format=file
+cd ..
+
+
+
+#####################
+### LOAD DATASETS ###
+#####################
+Rscript $script_path/00_load_data.R \
+--input_path $main/'data/SMARTseq2_RNA' \
+--dataset_metadata_path $main/'data/metadata.csv' \
+--species_use 'hsapiens' \
+--estimate_molecules_from_read_count 'True' \
+--mapping_threshold '2.5' \
+--sum_to_gene_level 'True' \
+--assay 'RNA' \
+--output_path $main/'analysis/1_qc' \
+2>&1 | tee $main/log/'00_load_data_log.txt'
+
+
+
+###########################
+### RUN QUALITY CONTROL ###
+###########################
+Rscript $script_path/01_qc_filter.R \
+--Seurat_object_path $main/'analysis/1_qc/raw_seurat_object.rds' \
+--columns_metadata $var_to_plot \
+--species_use 'hsapiens' \
+--remove_non_coding 'False' \
+--plot_gene_family 'RPS,RPL,MT-,HB,MT[RS]' \
+--remove_gene_family 'mito,MT[RN]' \
+--min_gene_count '5' \
+--min_gene_per_cell '200' \
+--normalization 'LogNormalize' \
+--assay 'RNA' \
+--output_path $main/analysis/1_qc \
+2>&1 | tee $main/log/'01_QC_log.txt'
+
+
+
+##############################################################
+### RUN DATA INTEGRATION, NORMALIZE AND GET VARIABLE GENES ###
+##############################################################
+Rscript $script_path/02_integrate.R \
+--Seurat_object_path $main/'analysis/1_qc/filt_seurat_object.rds' \
+--columns_metadata $var_to_plot \
+--regress $var_to_regress \
+--var_genes 'scran,0.001' \
+--integration_method 'cca,Sex' \
+--cluster_use 'NONE' \
+--assay 'RNA' \
+--output_path $main/'analysis/2_clustering' \
+2>&1 | tee $main/log/'02_integrate_log.txt'
+
+
+
+####################################
+### RUN DIMENSIONALITY REDUCTION ###
+####################################
+Rscript $script_path/03_dr_and_cluster.R \
+--Seurat_object_path $main/'analysis/2_clustering/seurat_object.rds' \
+--columns_metadata $var_to_plot \
+--regress $var_to_regress \
+--PCs_use 'top,50' \
+--var_genes 'scran,0.001' \
+--dim_reduct_use 'umap' \
+--k_nearest_neighbour '10' \
+--pre_dim_reduct 'pca' \
+--cluster_use 'Celltype,ILC1,ILC2,ILC3,NK,T' \
+--cluster_method 'louvain,hc,leiden' \
+--output_path $main/'analysis/2_clustering' \
+--assay 'cca' \
+2>&1 | tee $main/log/'03_dr_and_cluster_log.txt'
+
+
+
+###################################
+### RUN DIFFERENTIAL EXPRESSION ###
+###################################
+Rscript $script_path/04_diff_gene_expr.R \
+--Seurat_object_path $main/'analysis/2_clustering/seurat_object.rds' \
+--clustering_use 'HC_21' \
+--metadata_use 'none' \
+--exclude_cluster 'NONE' \
+--DGE_method "wilcox" \
+--only_positive "true" \
+--covariates "NULL" \
+--max_cells_per_ident "200" \
+--pvalue_threshold "0.01" \
+--logfc_threshold "0.25" \
+--assay 'RNA' \
+--output_path $main/'analysis/2_clustering/3_diff_expr' \
+2>&1 | tee $main/'log/4_diff_expr_log.txt'
+
+
+
+############################
+### CELL TYPE PREDICTION ###
+############################
+Rscript $script_path/cell_type_prediction.R \
+--Seurat_object_path $main/'analysis/2_clustering/seurat_object.rds' \
+--marker_lists $script_path/'../support_files/cell_markers/main_cell_types.csv' \
+--assay 'RNA' \
+--output_path $main/'analysis/2_clustering/cell_type_prediction' \
+2>&1 | tee $main/'log_sauronv1/cell_type_prediction_log.txt'
+
+
+
+# #Clusters 12, 16 and 20 were associated to Fibroblasts and APCs and were removed
+# Hierachical Clustering was used because the total number of clusters is easy to find
+####################################
+### RUN DIMENSIONALITY REDUCTION ###
+####################################
+Rscript $script_path/03_dr_and_cluster.R \
+--Seurat_object_path $main/'analysis/2_clustering/seurat_object.rds' \
+--columns_metadata $var_to_plot \
+--regress $var_to_regress \
+--PCs_use 'top,50' \
+--var_genes 'scran,0.001' \
+--dim_reduct_use 'umap' \
+--k_nearest_neighbour '10' \
+--pre_dim_reduct 'pca' \
+--cluster_use 'HC_21,1,2,3,4,5,6,7,8,9,10,11,13,14,15,17,18,19,21' \
+--cluster_method 'louvain,hc,leiden' \
+--output_path $main/'analysis/2_clustering_filt' \
+--assay 'cca' \
+2>&1 | tee $main/log/'03_dr_and_cluster_log.txt'
+
+
+
+########################################
+### RUN CLUSTER CORRELATION ANALYSIS ###
+########################################
+Rscript $script_path/'05_cluster_correlation.R' \
+--Seurat_object_path $main/'analysis/2_clustering_filt/seurat_object.rds' \
+--clustering_use 'HC_7' \
+--exclude_cluster 'NONE' \
+--merge_cluster '0.95,0.9,0.85,0.8,0.75,0.7' \
+--output_path $main/'analysis/2_clustering_filt/cluster_correlations' \
+2>&1 | tee $main/'log/4_clust_corr.txt'
+
+
+
+###################################
+### RUN DIFFERENTIAL EXPRESSION ###
+###################################
+Rscript $script_path/04_diff_gene_expr.R \
+--Seurat_object_path $main/'analysis/2_clustering_filt/seurat_object.rds' \
+--clustering_use 'HC_14' \
+--metadata_use 'none' \
+--exclude_cluster 'NONE' \
+--DGE_method "wilcox" \
+--only_positive "true" \
+--covariates "NULL" \
+--max_cells_per_ident "100" \
+--pvalue_threshold "0.1" \
+--logfc_threshold "0.1" \
+--assay 'RNA' \
+--output_path $main/'analysis/2_clustering_filt/3_diff_expr' \
+2>&1 | tee $main/'log/4_diff_expr_log.txt'
+
+
+
+########################
+### RUN VDJ ANALYSIS ###
+########################
+Rscript $script_path/VDJ_analysis.R \
+--Seurat_object_path $main/'analysis/2_clustering_filt/seurat_object.rds' \
+--VDJ_annotation_path $main/'data/SMARTseq2_VDJ_mixcr' \
+--columns_metadata 'Celltype' \
+--top_TCRs '10' \
+--paired_only 'false' \
+--only_coding_cdr3 'true' \
+--same_scale 'true' \
+--chains_use 'TRA,TRB,TRD,TRG' \
+--assay 'RNA' \
+--output_path $main/'analysis/2_clustering_filt/5_vdj_analysis_ig' \
+2>&1 | tee $main/log_sauronv1/'05_vdj_analysis_ig_log.txt'
+
+
+
+
+# conda deactivate
